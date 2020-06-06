fecfabaa8c3646fc81c162b04ff6a5eaf8632391
Paula Alves <paulafernandaalves@icloud.com>
2020-06-06 15:48:48 -0300

    created DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Propriedade.create(nome: 'Fazenda do João', especie: '1', cultivar: '1', area: 200, unidade:'m2')
+Propriedade.create(nome: 'Cooperativa da Maria', especie: '1', cultivar: '1', area: 200, unidade:'m2')
+Propriedade.create(nome: 'Sítio do Joaquim', especie: '1', cultivar: '1', area: 200, unidade:'m2')
